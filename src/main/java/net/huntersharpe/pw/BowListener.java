1bb910cf2ca11a89f77ec30f2a3bfab89b4035b1
BuildTools <huntersharpe@me.com>
2015-05-13 18:35:42 -0500

    Added event functionality.

--- src/main/java/net/huntersharpe/pw/BowListener.java
+++ src/main/java/net/huntersharpe/pw/BowListener.java
@@ -1,9 +1,11 @@
 package main.java.net.huntersharpe.pw;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.inventory.meta.ItemMeta;
 
 /**
  * Created by Hunter Sharpe on 5/13/15.
@@ -15,7 +17,9 @@ public class BowListener implements Listener{
         if(!(e.getEntity() instanceof Player)) return;
         Player p = (Player) e.getEntity();
         if(!p.hasPermission("pw.shoot")) return;
-
+        if(!e.getBow().getItemMeta().getDisplayName().equals("TNT Bow")) return;
+        if(!e.getBow().getItemMeta().getLore().equals("Shoots TNT")) return;
+        e.setProjectile(p.getWorld().spawnEntity(p.getLocation(), EntityType.PRIMED_TNT));
     }
 
 }
