6a51261fc4c0bc3ca5929c3a0be977b7cf12621b
BuildTools <unconfigured@null.spigotmc.org>
2015-05-13 18:17:20 -0500

    Initial commit

---
+++ src/main/java/net/huntersharpe/pw/ProjectileWeapons.java
@@ -0,0 +1,18 @@
+package main.java.net.huntersharpe.pw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Hunter Sharpe on 5/13/15.
+ */
+public class ProjectileWeapons extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+
+}
