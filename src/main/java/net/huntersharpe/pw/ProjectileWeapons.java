55f5c6bf3dec61dd423ecfed2431862213a21ab0
BuildTools <huntersharpe@me.com>
2015-05-13 19:27:32 -0500

    Added Custom Recipe

--- src/main/java/net/huntersharpe/pw/ProjectileWeapons.java
+++ src/main/java/net/huntersharpe/pw/ProjectileWeapons.java
@@ -1,6 +1,16 @@
 package main.java.net.huntersharpe.pw;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Hunter Sharpe on 5/13/15.
@@ -9,7 +19,9 @@ public class ProjectileWeapons extends JavaPlugin {
 
     public void onEnable(){
 
-
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new BowListener(), this);
+        addRecipes();
 
     }
 
@@ -17,4 +29,52 @@ public class ProjectileWeapons extends JavaPlugin {
 
     }
 
+    private void addRecipes(){
+        ShapedRecipe tntBow = new ShapedRecipe(tntBow());
+        tntBow.shape("", "", "",
+                " T ", " G ", " T ",
+                "", "", ""
+        );
+        tntBow.setIngredient('T', Material.TNT);
+        tntBow.setIngredient('G', Material.SULPHUR);
+        tntBow.shape(new String[]{"", "", "",
+                " T ", " G ", " T ",
+                "", "" , ""});
+        getServer().addRecipe(tntBow);
+
+        ShapedRecipe tntAmmo = new ShapedRecipe(tntAmmo());
+        tntAmmo.shape("", "", "",
+                " T ", " F ", " T ",
+                "", "", ""
+        );
+        tntAmmo.setIngredient('T', Material.TNT);
+        tntAmmo.setIngredient('G', Material.FIREBALL);
+        tntAmmo.shape(new String[]{"", "", "",
+                " T ", " G ", " T",
+                "", "", ""
+                });
+        getServer().addRecipe(tntAmmo);
+    }
+
+    private ItemStack tntBow(){
+        ItemStack tntBow = new ItemStack(Material.BOW);
+        ItemMeta tntMeta = tntBow.getItemMeta();
+        tntMeta.setDisplayName(ChatColor.RED + "TNT Bow");
+        List<String> s = new ArrayList<String>();
+        s.add(ChatColor.AQUA + "Shoots TNT");
+        tntMeta.setLore(s);
+        tntBow.setItemMeta(tntMeta);
+        return tntBow;
+    }
+
+    private ItemStack tntAmmo() {
+        ItemStack tntAmmo = new ItemStack(Material.ARROW);
+        ItemMeta tntMeta = tntAmmo.getItemMeta();
+        tntMeta.setDisplayName(ChatColor.GREEN + "TNT Bow Ammo");
+        List<String> s = new ArrayList<String>();
+        s.add(ChatColor.LIGHT_PURPLE + "Used for TNT Bow");
+        tntMeta.setLore(s);
+        tntAmmo.setItemMeta(tntMeta);
+        return tntAmmo;
+    }
 }
